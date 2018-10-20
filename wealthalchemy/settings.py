af12307a37d26a681d8f2c291ea378e43078cdce
anthony-mendola <anthonymendola@gmail.com>
2018-10-20 12:04:56 -0400

    convert to postgres

--- wealthalchemy/settings.py
+++ wealthalchemy/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'wealthalchemy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wealthalchemydb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
