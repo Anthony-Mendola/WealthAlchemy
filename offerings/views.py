628af10fed465a5a9ebd864dcd6289d74120db75
anthony-mendola <anthonymendola@gmail.com>
2018-10-20 00:05:54 -0400

    added static offerings page

--- offerings/views.py
+++ offerings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'offerings/offerings.html')
+
+def offering(request):
+  return render(request, 'offerings/offering.html')
+
+def search(request):
+  return render(request, 'offerings/search.html')
+
