9e23a110ebb9f5faab864d90461d63ed06e856be
anthony-mendola <anthonymendola@gmail.com>
2018-10-21 20:57:37 -0400

    added more display functionality to admin area

--- offerings/admin.py
+++ offerings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Offering
 
-admin.site.register(Offering)
+class OfferingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'is_open', 'minimum_investment', 'industry', 'offer_date', 'affiliate')
+  list__display_links = ('id', 'title')
+  list__filter = ('affiliate',)
+  list__editable = ('is_published',)
+  search_fields = ('title', 'description', 'industry', 'annual_interest', 'term', 'category', 'minimum_investment')
+  list__per_page = 25
+
+admin.site.register(Offering, OfferingAdmin)
