b19df6a6b291cddf411b4d16291f6ae97ebbb3a9
anthony-mendola <anthonymendola@gmail.com>
2018-10-21 21:02:49 -0400

    customized affiliate admin area

--- affiliates/admin.py
+++ affiliates/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Affiliate
 
-admin.site.register(Affiliate)
+class AffiliateAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'experience')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 25
+
+
+admin.site.register(Affiliate, AffiliateAdmin)
--- offerings/admin.py
+++ offerings/admin.py
@@ -4,10 +4,10 @@ from .models import Offering
 
 class OfferingAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'is_published', 'is_open', 'minimum_investment', 'industry', 'offer_date', 'affiliate')
-  list__display_links = ('id', 'title')
-  list__filter = ('affiliate',)
-  list__editable = ('is_published',)
+  list_display_links = ('id', 'title')
+  list_filter = ('affiliate',)
+  list_editable = ('is_published',)
   search_fields = ('title', 'description', 'industry', 'annual_interest', 'term', 'category', 'minimum_investment')
-  list__per_page = 25
+  list_per_page = 25
 
 admin.site.register(Offering, OfferingAdmin)
