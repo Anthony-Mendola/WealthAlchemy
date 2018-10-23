2bed04ab8e7ec25edca86369870ff384437d3926
anthony-mendola <anthonymendola@gmail.com>
2018-10-23 18:45:37 -0400

    setup index page to pull from database

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from affiliates.models import Affiliate
+from offerings.models import Offering
+
 def index(request):
-    return render(request, 'pages/index.html')
+    offerings = Offering.objects.order_by('-offer_date').filter(is_published=True)[:3]
+
+    context = {
+      'offerings': offerings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+  #get all affiliates
+    affiliates = Affiliate.objects.order_by('-hire_date')
+
+
+    #get affiliate of the month
+    mvp_affiliates = Affiliate.objects.all().filter(is_mvp=True)
+
+    context = {
+      'affiliates': affiliates,
+      'mvp_affiliates': mvp_affiliates
+    }
+    return render(request, 'pages/about.html', context)
 
