18e2441dc7198750ff0a7634d84a86745638166c
anthony-mendola <anthonymendola@gmail.com>
2018-10-22 21:48:29 -0400

    added pagination

--- offerings/views.py
+++ offerings/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Offering
 
 def index(request):
   offerings = Offering.objects.all()
 
+  paginator = Paginator(offerings, 6)
+  page = request.GET.get('page')
+  page_offerings = paginator.get_page(page)
+
   context = {
-    'offerings': offerings
+    'offerings': paged_offerings
   }
   return render(request, 'offerings/offerings.html', context)
 
-def offering(request):
+def offering(request, offering_id):
   return render(request, 'offerings/offering.html')
 
 def search(request):
