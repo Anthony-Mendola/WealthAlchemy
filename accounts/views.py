25e72d1fe3ea99a64b005336e13029144df6ccd2
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 20:48:19 -0400

    added dashboard template and links

--- accounts/views.py
+++ accounts/views.py
@@ -24,7 +24,6 @@ def register(request):
           messages.error(request, 'That email is being used')
           return redirect('register')
         else:
-          # Looks good
           user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
           # Login after register
           # auth.login(request, user)
