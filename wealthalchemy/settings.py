d26650862bd174437d4802dae960d491af533f12
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 23:33:20 -0400

    added local settings import

--- wealthalchemy/settings.py
+++ wealthalchemy/settings.py
@@ -148,3 +148,8 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_USER = 'wealthalchemy@gmail.com'
 # EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
