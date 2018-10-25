3d7f4e5026f0c638c442b540d9d323ef63bebb5d
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 21:03:25 -0400

    contact migration done, admin.py done

--- wealthalchemy/settings.py
+++ wealthalchemy/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'offerings.apps.OfferingsConfig',
     'affiliates.apps.AffiliatesConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
