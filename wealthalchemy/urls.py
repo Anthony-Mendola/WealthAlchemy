1c957b41e303b89b93db021fc945b536fc059fc2
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 21:23:57 -0400

    contact path

--- wealthalchemy/urls.py
+++ wealthalchemy/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('offerings/', include('offerings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
