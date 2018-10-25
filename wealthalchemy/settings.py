74fb0b1aa96bb3816eacf9370a362ebf265b4a33
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 22:28:12 -0400

    working on dashboard

--- wealthalchemy/settings.py
+++ wealthalchemy/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+#Email config
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'wealthalchemy@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS=True
