9f98e8402644c1f157dcb5ed1b2fcaad7f5840da
anthony-mendola <anthonymendola@gmail.com>
2018-10-24 20:19:39 -0400

    working on accounts views

--- wealthalchemy/urls.py
+++ wealthalchemy/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
-     path('offerings/', include('offerings.urls')),
+    path('offerings/', include('offerings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
